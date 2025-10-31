c38cf2d1cb4e2e7b60683837d180f277708fd5d0
shahd <shahdmohamed7377@gmail.com>
2025-10-31 21:56:26 +0200

    BFS done

---
+++ src/BFS.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class BFS {
+    Set<String> visited = new HashSet<>();
+    int searchdepth=0;
+
+
+    public List<Node> bfs(Node start) {
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(start);
+        visited.add(start.state);
+
+        while (!queue.isEmpty()) {
+
+            Node curr = queue.poll();
+
+            if (curr.state.equals("012345678")) {
+                return buildPath(curr);
+            }
+
+            List<Pair> neighbors = Successors.getSuccessors(curr.state);
+
+            for (Pair p : neighbors) {
+                searchdepth++;
+                if (!visited.contains(p.state)) {
+
+                    visited.add(p.state);
+
+                    Node child = new Node(p.state, curr, p.direction, 0, 0);
+
+                    queue.add(child);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static List<Node> buildPath(Node goal) {
+        List<Node> path = new ArrayList<>();
+        while (goal != null) {
+            path.add(goal);
+            goal = goal.parent;
+        }
+        Collections.reverse(path);
+        return path;
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,68 @@
 import java.util.List;
 
 public class Main {
+
+    static final String RESET = "\u001B[0m";
+    static final String CYAN = "\u001B[36m";
+    static final String WHITE = "\u001B[37m";
+    static final String GREEN = "\u001B[32m";
+    static final String BLACK = "\u001B[30m";
+
+    public static void printBoard(String state, int step, String move) {
+
+        System.out.println(GREEN + "Step " + step +
+                (move != null ? " (Move: " + move + ")" : "") + RESET);
+
+        System.out.println(WHITE + "+---+---+---+" + RESET);
+
+        for (int i = 0; i < 9; i++) {
+
+            char c = state.charAt(i);
+            if (c == '0') {
+                System.out.print("|" + BLACK + "   " + RESET);
+            } else {
+                System.out.print("| " + CYAN + c + RESET + " ");
+            }
+
+            if (i % 3 == 2) {
+                System.out.println("|");
+                System.out.println(WHITE + "+---+---+---+" + RESET);
+            }
+        }
+
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
+        String initialState = "876543210";
 
-        //Successors s = new Successors();
-        List<Pair> test = Successors.getSuccessors("283164705");
-        List<Pair> test2 = Successors.getSuccessors("203164785");
+        Node start = new Node(initialState, null, null, 0, 0);
 
-        for (Pair i : test){
-            System.out.println(i.getDirection());
-            System.out.println(i.getState());
-            System.out.println("\n");
+
+        BFS solver = new BFS();
+
+        long startTime = System.currentTimeMillis();
+        List<Node> path = solver.bfs(start);
+        long endTime = System.currentTimeMillis();
+
+        if (path == null) {
+            System.out.println("No solution found!");
+            return;
         }
-        System.out.println("-----------------");
 
-        for (Pair i : test2){
-            System.out.println(i.getDirection());
-            System.out.println(i.getState());
-            System.out.println("\n");
+        System.out.println("\n==== BFS Solution Path ====\n");
+
+        for (int i = 0; i < path.size(); i++) {
+            Node n = path.get(i);
+            printBoard(n.state, i, n.action);
         }
+
+        System.out.println("Path cost: " + path.size());
+        System.out.println("Number of Explored Nodes : " + solver.visited.size());
+        System.out.println("Search Depth: " + path.size());
+        System.out.println("Time: " + (endTime-startTime) + " ms");
+
 
     }
 }
